4a4a9aed45028a520730c1f355e9e920a93794d7
Thirdy <163488855+thoffical@users.noreply.github.com>
2025-10-22 12:57:16 +0800

    Correct Python

--- addon.py
+++ addon.py
@@ -3,30 +3,30 @@ import xbmcgui
 import os
 import shutil
 import xbmc
-import xbmcgui
 from xbmcvfs import translatePath
 import platform 
 
-addon       = xbmcaddon.Addon()
-addonname   = addon.getAddonInfo('name')
+def clear_kodi_data():
+    addon = xbmcaddon.Addon()
+    addonname = addon.getAddonInfo('name')
+    
+    # Define the path to the Kodi userdata
+    kodi_data_path = translatePath('special://userdata/')  
 
-      # Confirm action with the user
+    # Confirm action with the user
+    dialog = xbmcgui.Dialog()
+    if dialog.yesno(
+        "Are You Sure? This malware teaches security and Python!",
+        f"This will also erase all data in:\n{kodi_data_path}\nDo you want to continue?",
+    ):
+        # Alert user about the irreversible action
+        line1 = "(NO TURNING BACK!)"
+        xbmcgui.Dialog().ok(addonname, line1)
+
         dialog = xbmcgui.Dialog()
         if dialog.yesno(
-            "Are You Sure? This malware teaches security and Python!",
-            f"This will also erase all data in:\n{kodi_data_path}\nDo you want to continue?",
-        ):
-
-# Set a string variable to use 
-line1 = "(NO TURNING BACK!)"
-
-# Launch a dialog box in kodi showing the string variable 'line1' as the contents
-xbmcgui.Dialog().ok(addonname, line1)
-
-dialog = xbmcgui.Dialog()
-        if dialog.yesno(
-          "XBMCDrop",
-          f"< Go to trash | Be secured >",
+            "XBMCDrop",
+            f"< Go to trash | Be secured >",
         ):
             # Delete userdata contents
             for item in os.listdir(kodi_data_path):
@@ -43,16 +43,19 @@ dialog = xbmcgui.Dialog()
             xbmcgui.Dialog().ok("Success", "Kodi data has been erased. Please restart Kodi.")
         else:
             xbmcgui.Dialog().ok("Canceled", "No changes were made.")
+    else:
+        xbmcgui.Dialog().ok("Canceled", "No changes were made.")
+
+if __name__ == "__main__":
+    try:
+        # Check for the platform and modify behavior if necessary
+        system = platform.system()
+        if system in ['Darwin', 'Windows', 'Linux', 'Android']:
+            clear_kodi_data()
+        elif system == 'iOS':
+            xbmcgui.Dialog().ok("Error", "Clearing Kodi data is not supported on iOS.")
+        else:
+            xbmcgui.Dialog().ok("Error", f"Unsupported platform: {system}")
     except Exception as e:
         xbmcgui.Dialog().ok("Error", f"An error occurred: {str(e)}")
         xbmc.log(f"Critical Error: {str(e)}", level=xbmc.LOGERROR)
-
-if __name__ == "__main__":
-    # Check for the platform and modify behavior if necessary
-    system = platform.system()
-    if system in ['Darwin', 'Windows', 'Linux', 'Android']:
-        clear_kodi_data()
-    elif system == 'iOS':
-        xbmcgui.Dialog().ok("Error", "Clearing Kodi data is not supported on iOS.")
-    else:
-        xbmcgui.Dialog().ok("Error", f"Unsupported platform: {system}")
